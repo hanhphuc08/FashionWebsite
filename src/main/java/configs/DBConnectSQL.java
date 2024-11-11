7f03d7e1fc68f6b0ba5bf21b03f3f0ea789e2ff7
tuong2411 <tuongporo9x2004@gmail.com>
2024-11-12 00:17:14 +0700

    Test

--- src/main/java/configs/DBConnectSQL.java
+++ src/main/java/configs/DBConnectSQL.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnectSQL {
-	private final String serverName = "MSI";
-	private final String dbName = "ltwebct4";
+	private final String serverName = "LAPTOP-3N644IDG";
+	private final String dbName = "DoAnCNTT";
 	private final String portNumber = "1433";
 	private final String instance = "";
 	private final String userID = "sa";
