3cc9836103b6e1cb297daa61de9085fcbff1e024
MSI\ANH THU <truongthianhthu2011@gmail.com>
2024-11-11 09:51:46 +0700

    Detail Controller

--- src/main/java/controllers/CategoryDetailController.java
+++ src/main/java/controllers/CategoryDetailController.java
@@ -2,37 +2,12 @@ package controllers;
 
 import java.io.IOException;
 
-<<<<<<< HEAD
-
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.MultipartConfig;
-=======
-import jakarta.servlet.ServletException;
->>>>>>> 3bec862268ae7b3b7538cf6fa0cfb1f11d0d5b98
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-<<<<<<< HEAD
-
-@MultipartConfig(fileSizeThreshold = 1024 * 1024, 
-			maxFileSize = 1024 * 1024 * 5, maxRequestSize = 1024 * 1024 * 5 * 5 )
-
-
-@WebServlet(urlPatterns = {"/categories/detail", "/user/categories/detail"})
-public class CategoryDetailController extends HttpServlet{
-
-	private static final long serialVersionUID = 1L;
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
-		req.getRequestDispatcher("/views/user/categoryDetail.jsp").forward(req,  resp);
-
-	}
-}
-=======
 @WebServlet(urlPatterns= {"/categoryDetail"})
 public class CategoryDetailController extends HttpServlet {
 
@@ -49,4 +24,3 @@ public class CategoryDetailController extends HttpServlet {
 	
 
 }
->>>>>>> 3bec862268ae7b3b7538cf6fa0cfb1f11d0d5b98
